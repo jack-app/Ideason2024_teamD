0fa59230d7beede376e96e50aba3c1561575fd4f
ゆき <kawamura.yuki.v3@s.mail.nagoya-u.ac.jp>
2024-06-02 13:44:45 +0900

    home title

--- front/src/content/home.js
+++ front/src/content/home.js
@@ -9,10 +9,11 @@ function Home() {
     <div>
       <Header />
       <nav>
+      <p className='title'>Penguin's Puzzle and Audio Production</p>
       <Button text="Game" to="/game" styleType="default" />
       <Button text="SoundTest" to="/st" styleType="default" />
       <Button text="Rules" to="/rules" styleType="default" />
-      <p className='title'>Penguin's Puzzle and Audio Production</p>
+      
       </nav>
     </div>
   );
