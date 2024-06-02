be36005e87582f5dc795f3f7567ca02664251481
nishiyan <nishida.haruto.h6@s.mail.nagoya-u.ac.jp>
2024-06-02 12:40:01 +0900

    背景を作ろうとした

--- front/src/content/home.js
+++ front/src/content/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/header.jsx';
 import Button from '../components/button.jsx';
+import '../stylesheet/background.css';
 
 function Home() {
   return (
