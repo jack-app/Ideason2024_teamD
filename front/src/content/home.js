ee5b464bdac98e21f8d1c7452ba0efea9216004a
nishiyan <nishida.haruto.h6@s.mail.nagoya-u.ac.jp>
2024-06-02 15:17:24 +0900

    タイトル画面の画像をいじった

--- front/src/content/home.js
+++ front/src/content/home.js
@@ -9,11 +9,28 @@ function Home() {
     <div>
       <Header />
       <nav>
+        <div className='box'>
+         <div className='one'>
       <Button text="Game" to="/game" styleType="default" />
+         </div>  
+         <div className='two'>
       <Button text="SoundTest" to="/st" styleType="default" />
+         </div>
+         <div className='three'>
       <Button text="Rules" to="/rules" styleType="default" />
+         </div>
+        </div>
+         
+        <div className='penguin'>
+          <img src="../../public/image/penguin.png"></img>
+        </div>
+        <div className='onnpu'>
+          <img src="../../public/image/onnou.png"></img>
+        </div>
+          
+        
       <p className='title'>Penguin and </p>
-      </nav>
+     </nav>
     </div>
   );
 }
