2f4724847d6b78d5ef769798e871d6f535dfe35b
itoxu110 <yitenghuixi@gmail.com>
2024-06-04 21:36:58 +0900

    重なっているテキストを削除

--- front/src/content/rules.js
+++ front/src/content/rules.js
@@ -8,9 +8,7 @@ function Home() {
   return (
     <div>
       <Header />
-      <div className='box'>
-       <h1 className='text'>ルール説明</h1>
-      </div>
+      <div className="rules">
       <h2>ゲームの遊び方</h2>
       <p>横に2個同じブロックを並べて音を消そう！十字キーの方向に動くけど上には動かせないよ。</p>
       <p>ブロックが消えると音が鳴るよ！</p>
@@ -25,6 +23,7 @@ function Home() {
       <p>消したブロックで曲ができるよ！</p>
       <h2>アルファベットは何？</h2>
       <p>C,G,Am,Em,Fは高さが違う音を表してるよ。C→G→Am→Em→F→C→F→Gを繰り返すことをカノン進行と言うよ。</p>
+      </div>
     </div>
   );
 }
